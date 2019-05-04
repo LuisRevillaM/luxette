e404a909134dbe03fabf600dbaf2272c6eb69b43
Luis Revilla <luisrevillameza@gmail.com>
2019-05-04 10:56:07 -0400

    testimonials

--- src/App.js
+++ src/App.js
@@ -23,34 +23,15 @@ const Header = () => {
 const Landing = () => {
   return (
     <div className="title">
-      <div>Luxury bachelorettes. Planned by us, tailored to you.</div>
-      <div className="main-cta">
+      <div>Luxury bachelorettes.</div>
+
+      <div>Planned by us, tailored to you.</div>
+      <a href="mailto:lauren.e.blocker@gmail.com" className="main-cta">
         <div>See destinations</div>
         <div>
           <img className="down" src={down} alt="down" />
         </div>
-      </div>
-    </div>
-  );
-};
-
-const Attribute = props => {
-  return (
-    <div className="attr">
-      <img className="attr-icon" src={check} alt="check" />
-      <div>{props.attribute}</div>
-    </div>
-  );
-};
-
-const AttributeTitle = () => <div className="attribute-title" />;
-
-const Attributes = () => {
-  return (
-    <div className="attribute-section">
-      <Attribute attribute="Customized" />
-      <Attribute attribute="Curated" />
-      <Attribute attribute="Perfectly executed!" />
+      </a>
     </div>
   );
 };
@@ -140,7 +121,38 @@ const MainImage = () => (
   </div>
 );
 
-const Separator = () => <div className="separator" />;
+const Testimonials = () => (
+  <div className="testimonials">
+    <div className="how-works-title">What our clients are saying</div>
+    <p>
+      I had a wonderful experience planning my best friend's bachelorette party
+      with Lauren! From beginning to end, Lauren was so meticulous about making
+      sure that my vision for the event came to life, and that everything went
+      off without a hitch. It was clear Lauren knows the best of the best when
+      it comes to dining and nightlife in New Orleans - all of her restaurant
+      recommendations were fantastic. I also loved all of the special touches
+      she put in place while we were on the ground. Our entire party was blown
+      away! Having her by my side was a HUGE help and I couldn't have done it
+      without her (especially while working 2 jobs!). I'd highly recommend
+      Lauren and Luxette for any travel or event planning needs. 5 stars!!
+    </p>
+    <p className="client">- Tiffany M, MOH, New Orleans</p>
+    <p>
+      "I couldn't have imagined a better bachelorette trip than what Lauren (of
+      Luxette) and my Maid of Honor organized for me in New Orleans! The
+      itinerary was so well thought out and SO much fun—amazing restaurants and
+      bars, super fun activities, and never a dull moment with my favorite
+      ladies. Every detail was impeccable, including the communication with the
+      entire group beforehand (complete with a detailed itinerary and outfit
+      planner), the way my room was decorated when I arrived, the hotel choice,
+      the pre-arrangement of details at each restaurant, and fun and hilarious
+      activities every step of the way! I would 1,000% recommend Luxette to
+      anyone who wants to take their next friends trip (bachelorette or
+      otherwise) to the next level!"
+    </p>
+    <p className="client">- Aly T., Bride, New Orleans</p>
+  </div>
+);
 
 class App extends Component {
   render() {
@@ -151,6 +163,7 @@ class App extends Component {
         <Landing />
         <HowWorks />
         <Destinations />
+        <Testimonials />
       </div>
     );
   }
